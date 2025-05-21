e822210ab65a56f908f9279e746d9e3aa45d80a3
Yuu Aoki <231205001@ccmailg.meijo-u.ac.jp>
2025-05-21 10:10:11 +0900

    ミス

---
+++ Assets/Script/move_camera.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_camera : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+
+    Vector3 currentPos;//現在のカメラ位置
+    Vector3 pastPos;//過去のカメラ位置
+
+    Vector3 diff;//移動距離
+    [SerializeField] public Quaternion hRotation;      // カメラの水平回転
+
+    private void Start()
+    {
+        //回転の初期化
+        hRotation = Quaternion.identity;                // 水平回転(Y軸を軸とする回転)は、無回転
+        //最初のプレイヤーの位置の取得
+        pastPos = player.transform.position;
+    }
+    void Update()
+    {
+        //------カメラの移動------
+
+        //プレイヤーの現在地の取得
+        currentPos = player.transform.position;
+
+        diff = currentPos - pastPos;
+
+        transform.position = Vector3.Lerp(transform.position, transform.position + diff, 1.0f);//カメラをプレイヤーの移動差分だけうごかす
+
+        pastPos = currentPos;
+
+
+        //------カメラの回転------
+
+        // マウスの移動量を取得
+        float mx = Input.GetAxis("Mouse X") * 2.0f;
+        float my = Input.GetAxis("Mouse Y") * 1.5f;
+
+        hRotation *= Quaternion.Euler(0, mx, 0);
+
+        // X方向に一定量移動していれば横回転
+        if (Mathf.Abs(mx) > 0.01f)
+        {
+            // 回転軸はワールド座標のY軸
+            transform.RotateAround(player.transform.position, Vector3.up, mx);
+        }
+
+        // Y方向に一定量移動していれば縦回転
+        if (Mathf.Abs(my) > 0.01f)
+        {
+            // 回転軸はカメラ自身のX軸
+            transform.RotateAround(player.transform.position, transform.right, -my);
+        }
+    }
+}
---
+++ Assets/Script/move_player.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class move_player : MonoBehaviour
+{
+
+    [SerializeField] private Vector3 velocity;              // 移動方向
+    [SerializeField] private float moveSpeed = 5.0f;        // 移動速度
+    [SerializeField] private move_camera refCamera;  // カメラの水平回転を参照する用
+    [SerializeField] private float applySpeed = 0.2f; //回転速度
+    [SerializeField] private float buff;
+
+    void Start() {
+        buff = 1;
+    }
+
+    void Update()
+    {
+        // WASD入力から、XZ平面(水平な地面)を移動する方向(velocity)を得る
+        velocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.W) || Input.GetKey("up"))
+            velocity.z -= 1 * buff;
+        if (Input.GetKey(KeyCode.A) || Input.GetKey("left"))
+            velocity.x += 1 * buff;
+        if (Input.GetKey(KeyCode.S) || Input.GetKey("down"))
+            velocity.z += 1 * buff;
+        if (Input.GetKey(KeyCode.D) || Input.GetKey("right"))
+            velocity.x -= 1 * buff;
+
+        // 速度ベクトルの長さを1秒でmoveSpeedだけ進むように調整
+        velocity = velocity.normalized * moveSpeed * Time.deltaTime;
+
+        // いずれかの方向に移動している場合
+        if (velocity.magnitude > 0)
+        {
+            // プレイヤーの回転(transform.rotation)の更新
+            // 無回転状態のプレイヤーのZ+方向(後頭部)を、
+            // カメラの水平回転(refCamera.hRotation)で回した移動の反対方向(-velocity)に回す回転に段々近づける
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                    Quaternion.LookRotation(refCamera.hRotation * -velocity),
+                                                    applySpeed);
+
+            // プレイヤーの位置(transform.position)の更新
+            // カメラの水平回転(refCamera.hRotation)で回した移動方向(velocity)を足し込み
+            transform.position += refCamera.hRotation * velocity;
+        }
+    }
+
+}
+
